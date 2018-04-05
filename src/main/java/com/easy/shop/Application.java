f802b887b460af58b4cf093cfcd3002db23b487a
malviyabhinavgit <abhinavmalviya.nitrkl@gmail.com>
2018-04-05 21:54:26 +0100

    added space

--- src/main/java/com/easy/shop/Application.java
+++ src/main/java/com/easy/shop/Application.java
@@ -10,6 +10,7 @@ public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
